79df6ec05222efe98e3829b1cc91f1849e4ee9ed
BANGHO <BANGHO@DESKTOP-9BPI07G.home>
2022-10-16 18:07:46 -0500

    Aplicacion Jcalendar

--- src/main/java/menus/Registro.java
+++ src/main/java/menus/Registro.java
@@ -10,6 +10,7 @@ import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
+import com.toedter.calendar.JDateChooser;
 
 public class Registro extends JFrame {
 
@@ -18,6 +19,7 @@ public class Registro extends JFrame {
 	private JTextField textField_1;
 	private JTextField textField_2;
 	private JTextField textField_3;
+	private JTextField textField_4;
 
 	/**
 	 * Launch the application.
@@ -48,39 +50,56 @@ public class Registro extends JFrame {
 		contentPane.setLayout(null);
 		
 		textField = new JTextField();
-		textField.setBounds(156, 45, 140, 25);
+		textField.setBounds(156, 22, 140, 25);
 		contentPane.add(textField);
 		textField.setColumns(10);
 		
 		textField_1 = new JTextField();
-		textField_1.setBounds(156, 81, 140, 25);
+		textField_1.setBounds(156, 58, 140, 25);
 		contentPane.add(textField_1);
 		textField_1.setColumns(10);
 		
 		textField_2 = new JTextField();
-		textField_2.setBounds(156, 117, 140, 25);
+		textField_2.setBounds(156, 130, 140, 25);
 		contentPane.add(textField_2);
 		textField_2.setColumns(10);
 		
 		JLabel lblNewLabel = new JLabel("Nombre:");
-		lblNewLabel.setBounds(100, 50, 46, 14);
+		lblNewLabel.setBounds(100, 27, 46, 14);
 		contentPane.add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("Apellido:");
-		lblNewLabel_1.setBounds(100, 86, 46, 14);
+		lblNewLabel_1.setBounds(100, 63, 46, 14);
 		contentPane.add(lblNewLabel_1);
 		
-		JLabel lblNewLabel_2 = new JLabel("Tipo de Nro. de Documento:");
-		lblNewLabel_2.setBounds(10, 122, 136, 14);
+		JLabel lblNewLabel_2 = new JLabel("Tipo de Documento:");
+		lblNewLabel_2.setBounds(50, 99, 96, 14);
 		contentPane.add(lblNewLabel_2);
 		
 		textField_3 = new JTextField();
-		textField_3.setBounds(156, 153, 140, 25);
+		textField_3.setBounds(156, 166, 140, 25);
 		contentPane.add(textField_3);
 		textField_3.setColumns(10);
 		
 		JLabel lblNewLabel_3 = new JLabel("Sexo:");
-		lblNewLabel_3.setBounds(110, 158, 36, 14);
+		lblNewLabel_3.setBounds(118, 171, 28, 14);
 		contentPane.add(lblNewLabel_3);
+		
+		JDateChooser dateChooser = new JDateChooser();
+		dateChooser.setBounds(156, 202, 140, 25);
+		contentPane.add(dateChooser);
+		
+		textField_4 = new JTextField();
+		textField_4.setBounds(156, 94, 140, 25);
+		contentPane.add(textField_4);
+		textField_4.setColumns(10);
+		
+		JLabel lblNewLabel_4 = new JLabel("Nro. de documento:");
+		lblNewLabel_4.setBounds(50, 135, 96, 14);
+		contentPane.add(lblNewLabel_4);
+		
+		JLabel lblNewLabel_5 = new JLabel("Fecha de nacimiento:");
+		lblNewLabel_5.setBounds(50, 213, 104, 14);
+		contentPane.add(lblNewLabel_5);
 	}
 }
